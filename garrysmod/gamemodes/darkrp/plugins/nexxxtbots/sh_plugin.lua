f8070e8500299347024839f1e2413d0bc8be19e1
Daniil Akabenko <51786832+Kek1ch@users.noreply.github.com>
2019-06-17 06:00:37 +0300

    Update sh_plugin.lua

--- garrysmod/gamemodes/darkrp/plugins/nexxxtbots/sh_plugin.lua
+++ garrysmod/gamemodes/darkrp/plugins/nexxxtbots/sh_plugin.lua
@@ -1,6 +1,7 @@
 local PLUGIN = PLUGIN
 PLUGIN.name = "NeXXXbots"
 PLUGIN.author = "Kek1ch x WickedRabbit x DrodA x Petrenko x Spenser :sunglasses:"
+--Некоторый код не выкладывался по просьюе Спебсера
 PLUGIN.desc = "Кажется, что весь код в sv_hooks.lua, которые не тронул scripthook"
 
 nut.util.include("sv_plugin.lua")
