a64ead374c9b6e5a8bff2e53558d0e82c24ded6d
Daniil Akabenko <51786832+Kek1ch@users.noreply.github.com>
2020-08-19 19:12:40 +0300

    Update sh_plugin.lua

--- garrysmod/gamemodes/darkrp/plugins/twentyonepoints/sh_plugin.lua
+++ garrysmod/gamemodes/darkrp/plugins/twentyonepoints/sh_plugin.lua
@@ -2,7 +2,7 @@
 
 PLUGIN.name = "Twenty One Point"
 PLUGIN.author = "Kek1ch&DrodA"
-PLUGIN.desc = "Если вам нужно 21 очко в 1000 строк, то пишите vk.com/mamonov1337"
+PLUGIN.desc = "21 очко в 1000 строк"
 
 nut.util.include("sv_plugin.lua")
 
